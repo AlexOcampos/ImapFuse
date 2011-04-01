edd07e9e691597c08ee0041943ca7b6109e98fa0
Alex Ocampos <alexocamposveiga@gmail.com>
2011-04-01 13:08:30 +0200

    Method search_folder which is used in ImapFuse_readdir

--- imapFuse.cpp
+++ imapFuse.cpp
@@ -33,16 +33,19 @@ static int port_n;
 static string user;
 static string pass;
 static bool usessl;
-static list<Folder> folder_list;	// List of folders
+static list<Folder> folder_list;	// List of folders in folder root
 
+// Methods header
+Folder* search_folder(char * name);
+
+// FUSE methods
 /**
  * This metod obtain the attributes of a file.
  * @param path The path of the file
  * @param stbuf The place where the stat structure with his attributes will be save
  * @return 0 on success
  */
-static int ImapFuse_getattr(const char *path, struct stat *stbuf)
-{
+static int ImapFuse_getattr(const char *path, struct stat *stbuf) {
 	int res = 0;
 	memset(stbuf, 0, sizeof(struct stat));
 	if (strcmp(path, "/") == 0) {
@@ -52,33 +55,18 @@ static int ImapFuse_getattr(const char *path, struct stat *stbuf)
 		stbuf->st_mode = S_IFREG | 0444;
 		stbuf->st_nlink = 1;
 		stbuf->st_size = strlen(hello_str);
-	} else {
+	} else { // Si es una carpeta
 		//res = -ENOENT;
 		stbuf->st_mode = S_IFDIR | 0755;
+		stbuf->st_nlink = 2;
+		stbuf->st_uid = getuid();
+		stbuf->st_gid = getgid();
+		stbuf->st_size = 0;
+		stbuf->st_blksize = 4096;
+		stbuf->st_blocks = stbuf->st_size/stbuf->st_blksize+((stbuf->st_size%stbuf->st_blksize)? 1:0);
 	}
 
 	return res;
-}
-
-static int ImapFuse_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
-			 off_t offset, struct fuse_file_info *fi)
-{
-	(void) offset;
-	(void) fi;
-
-	if (strcmp(path, "/") != 0) {// Si no estamos en el directorio raiz del email
-		return -ENOENT;
-	} else {
-		filler(buf, ".", NULL, 0);
-		filler(buf, "..", NULL, 0);
-		list<Folder>::iterator itera = folder_list.begin();
-		while(itera != folder_list.end()) {
-			filler(buf, ((*itera).get_Folder_Name()).c_str(), NULL, 0);
-			itera++;
-		}
-		filler(buf, hello_path + 1, NULL, 0);
-	}
-	return 0;
 }
 
 static int ImapFuse_open(const char *path, struct fuse_file_info *fi)
@@ -92,8 +80,7 @@ static int ImapFuse_open(const char *path, struct fuse_file_info *fi)
 	return 0;
 }
 
-static int ImapFuse_read(const char *path, char *buf, size_t size, off_t offset,
-		      struct fuse_file_info *fi)
+static int ImapFuse_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi)
 {
 	size_t len;
 	(void) fi;
@@ -111,6 +98,44 @@ static int ImapFuse_read(const char *path, char *buf, size_t size, off_t offset,
 	return size;
 }
 
+/**
+ * This method obtains all the archives in path.
+ * @param path The path of the file
+ * @param buf The structure which will be filling with folder names
+ * @param filler The filling function
+ * @param offset The offset between directory entries
+ * @param fi The info of fuse file
+ */
+static int ImapFuse_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi) {
+	(void) offset;
+	(void) fi;
+
+	if (strcmp(path, "/") != 0) {// Si no estamos en el directorio raiz del email
+		// Mirar si está en la lista de folders
+		Folder* f;
+		f = search_folder((char *)path);
+		if (f != NULL) {
+			filler(buf, ".", NULL, 0);
+			filler(buf, "..", NULL, 0);
+			filler(buf, "holahola", NULL, 0); // DEBUG
+			return 0;
+		}
+		
+		return -ENOENT;
+	} else {
+		filler(buf, ".", NULL, 0);
+		filler(buf, "..", NULL, 0);
+		list<Folder>::iterator itera = folder_list.begin();
+		while(itera != folder_list.end()) {
+			filler(buf, ((*itera).get_Folder_Name()).c_str(), NULL, 0);
+			itera++;
+		}
+		filler(buf, hello_path + 1, NULL, 0);
+	}
+	return 0;
+}
+
+// Useful Methods
 /**
  * Add folder to folder list
  * @param F A folder object
@@ -142,11 +167,29 @@ void create_folder_list( list<string> folder_names, string folder_root) {
 }
 
 /**
+ * Search for a folder in folder_list.
+ * @param name Name of folder.
+ * @return The folder object.
+ */
+Folder* search_folder(char * name) {
+	list<Folder>::iterator itera = folder_list.begin();
+	while(itera != folder_list.end()) {
+		if (strcmp(name+1, ((*itera).get_Folder_Name()).c_str()) == 0) {
+			return &(*itera);
+		}
+		itera++;
+	}
+	
+	return NULL;
+}
+
+// Imap Methods
+/**
  * Obtain a list of folders
  * @param folder_root name of folder root
  * @return SUCCESS or LIST_FAILED
  */ 
-int get_folders_list(string folder_root) {
+int get_folders_list_from_server(string folder_root) {
 	int return_code;
 	list<string> folder_names;
 	return_code = getIMAPFolders(connection, folder_names, folder_root );
@@ -182,8 +225,7 @@ int login(void) {
 /**
  * Main
  */
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 	FuseDispatcher *dispatcher;
 	dispatcher = new FuseDispatcher();
 	
@@ -193,7 +235,7 @@ int main(int argc, char *argv[])
 	dispatcher->set_read	(&ImapFuse_read);
 	
 	login();
-	get_folders_list("");
+	get_folders_list_from_server("");
 	
 	return fuse_main(argc, argv, (dispatcher->get_fuseOps()), NULL);
 }
